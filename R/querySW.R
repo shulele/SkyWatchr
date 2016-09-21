6a6bdb642b735ae6dab5a83fe652c042720e3d99
amsantac <amsantac@gmail.com>
2016-09-21 10:11:58 -0500

    removed old querySW2 function from querySW.R

--- R/querySW.R
+++ R/querySW.R
@@ -7,26 +7,3 @@ querySW <- function(level = "", coordinates = "", times = "", api_key = api_key)
   
   return(content(query))
 }
-
-# 
-# querySW2 <- function(level = NULL, coordinates = NULL, times = NULL, api_key){
-#   
-#   base_url <- "https://cqh77pglf1.execute-api.us-west-2.amazonaws.com/prod/data"
-#   
-#   if (!is.null(level)){
-#     base_url <- paste0(base_url, "/level/", level)
-#     }
-#   if (!is.null(coordinates)){
-#     base_url <- paste0(base_url, "/location/", coordinates)
-#     }
-#   if (!is.null(times)){
-#     base_url <- paste0(base_url, "/time/", times)
-#     }
-#                        
-#   sample <- GET(paste0(base_url, "?api_key=", api_key))
-# 
-# 
-#   return(content(sample))
-# }
-
-
