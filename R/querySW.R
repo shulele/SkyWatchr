232146b6d33018e7e3345c26e0a1bf3453c6ae0a
amsantac <amsantac@gmail.com>
2016-09-21 14:43:07 -0500

    added new setup oauth function and package .Rd file

--- R/querySW.R
+++ R/querySW.R
@@ -1,6 +1,6 @@
-querySW <- function(level = "", coordinates = "", times = "", api_key = api_key){
+querySW <- function(level = NULL, coordinates, times = NULL, api_key){
   
-  if (times == "") times <- Sys.Date()
+  if (is.null(times)) times <- Sys.Date()
   
   query <- GET(paste0("https://cqh77pglf1.execute-api.us-west-2.amazonaws.com/prod/data/level/", level, "/location/",
                        coordinates, "/time/", times, "?api_key=", api_key))
