19408d4614e4e2f7d0a3100556e818fbbc1c80c7
amsantac <amsantac@gmail.com>
2016-09-23 09:42:35 -0500

    fixed typo

--- R/querySW.R
+++ R/querySW.R
@@ -5,7 +5,7 @@ querySW <- function(api_key, level = NULL, coordinates, times = NULL){
   URL <- paste0("https://api.skywatch.co/data/level/", level, "/location/",
                 coordinates, "/time/", times, "?")
   
-  query <- GET(URL, add_headers('Accept' = 'application/json', 'x-api-key' = api_key)))
+  query <- GET(URL, add_headers('Accept' = 'application/json', 'x-api-key' = api_key))
   
   return(content(query))
 }
