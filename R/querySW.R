334f9cfe608ab912935d6dbeb76bccacfd6b99eb
amsantac <amsantac@gmail.com>
2016-09-23 11:22:23 -0500

    added new search fields for API 0.3

--- R/querySW.R
+++ R/querySW.R
@@ -1,11 +1,17 @@
-querySW <- function(api_key, level = NULL, coordinates, times = NULL){
+querySW <- function(api_key, time_period = NULL, coordinates, instrument_satellite = NULL, data_level = NULL, 
+                    max_resolution = NULL, max_cloudcover = NULL, wavelength_band = NULL){
   
-  if (is.null(times)) times <- Sys.Date()
+  if (is.null(time_period)) time_period <- Sys.Date()
   
-  URL <- paste0("https://api.skywatch.co/data/level/", level, "/location/",
-                coordinates, "/time/", times, "?")
+  URL <- paste0("https://api.skywatch.co/data", "/time/", time_period, "/location/", coordinates)
   
-  query <- GET(URL, add_headers('Accept' = 'application/json', 'x-api-key' = api_key))
+  if(!is.null(instrument_satellite)) URL <- paste0(URL, "/source/", instrument_satellite)
+  if(!is.null(data_level)) URL <- paste0(URL, "/level/", data_level)
+  if(!is.null(max_resolution)) URL <- paste0(URL, "/resolution/", max_resolution)
+  if(!is.null(max_cloudcover)) URL <- paste0(URL, "/cloudcover/", max_cloudcover)
+  if(!is.null(wavelength_band)) URL <- paste0(URL, "/band/", wavelength_band)
+
+  query <- GET(paste0(URL, "?"), add_headers('Accept' = 'application/json', 'x-api-key' = api_key))
   
   return(content(query))
 }
