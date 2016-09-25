bcefe75c6aba3eb2202e66b8506fb1abd992375f
amsantac <amsantac@gmail.com>
2016-09-25 17:49:50 -0500

    added support for Spatial objects for coordinates argument in querySW function

--- R/querySW.R
+++ R/querySW.R
@@ -3,6 +3,8 @@ querySW <- function(api_key, time_period = NULL, coordinates, instrument_satelli
   
   if (is.null(time_period)) time_period <- Sys.Date()
   
+  if(is(coordinates, "Spatial")) coordinates <- paste0(bbox(coordinates), collapse = ",")
+
   URL <- paste0("https://api.skywatch.co/data", "/time/", time_period, "/location/", coordinates)
   
   if(!is.null(instrument_satellite)) URL <- paste0(URL, "/source/", instrument_satellite)
@@ -15,12 +17,12 @@ querySW <- function(api_key, time_period = NULL, coordinates, instrument_satelli
   
   res <- content(query)
   
-    res <- as.data.frame(do.call(rbind, lapply(res, unlist)))
-    res[] <- lapply(res, as.character)
-    res$size <- round(as.numeric(res$size)/1e3, 1)
-    res$cloud_cover <- as.numeric(res$cloud_cover)
-    res$resolution <- as.numeric(res$resolution)
-    colnames(res)[which(colnames(res) == 'size')] <- 'size_kb'
+  res <- as.data.frame(do.call(rbind, lapply(res, unlist)))
+  res[] <- lapply(res, as.character)
+  res$size <- round(as.numeric(res$size)/1e3, 1)
+  res$cloud_cover <- as.numeric(res$cloud_cover)
+  res$resolution <- as.numeric(res$resolution)
+  colnames(res)[which(colnames(res) == 'size')] <- 'size_kb'  
 
   if(output == "html"){
 
