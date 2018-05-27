6d766b212c28c2f7e5b88a92403a2b6fe216044b
amsantac <amsantac@gmail.com>
2018-05-26 21:05:33 -0500

    change how names for files downloaded by downloadSW() are constructed

--- R/downloadSW.R
+++ R/downloadSW.R
@@ -1,4 +1,4 @@
-downloadSW <- function(x, subset, api_key = NULL){
+downloadSW <- function(x, subset, file_name = "download_path", api_key = NULL){
   
   if (is.null(api_key)) {
     api_key <- getOption("SkyWatchr.apikey")
@@ -19,9 +19,12 @@ downloadSW <- function(x, subset, api_key = NULL){
   for(i in 1:nrow(x)){
     cat(paste0("Downloading ", i, "/", nrow(x), " ...", "\n"))
     urli <- x[i, "download_path"]
-    file_name <- paste0(x[i, "source"], "_", strsplit(urli, "/")[[1]][5], "_", x[i, "band"],  "_", strsplit(urli, "/")[[1]][4], ".", 
-                        x[i, "type"])
-    GET(x[i, "download_path"], add_headers('x-api-key' = api_key), write_disk(file_name))
+    # file_name <- paste0(x[i, "source"], "_", strsplit(urli, "/")[[1]][5], "_", x[i, "band"],  "_", strsplit(urli, "/")[[1]][4], ".", 
+    #                     x[i, "type"])
+    if(file_name == 'download_path') filename <- paste0(gsub(":", "_", gsub("\\.", "_", gsub("/", "_", dirname(urli)))), "_", basename(urli))
+    if(file_name == 'basename') filename <- basename(urli) # WARNING: this will throw error if a file with the same basename already exists
+    
+    GET(x[i, "download_path"], add_headers('x-api-key' = api_key), write_disk(filename))
   }
   cat(paste0("\n", "All requested files have been downloaded to: ", getwd()))
 }
